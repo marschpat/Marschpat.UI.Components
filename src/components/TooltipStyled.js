bc18adb7d04c07ac95abdcf3b78e313fc4188a7f
stefanstifter <stefan.stifter@saswebdev.com>
2021-06-11 10:29:23 +0200

    Add TooltipStyled component for testing purposes

---
+++ src/components/TooltipStyled.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const TooltipStyled = props => {
+    return (
+        <Tooltip title={<span className="text-lg leading-snug">{props.title}</span>}>
+            {props.children}
+        </Tooltip>
+    );
+}
+
+export default TooltipStyled;
