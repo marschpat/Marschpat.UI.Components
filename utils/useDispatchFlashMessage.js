1f5e221ce9b0170c879f40a320f1ce5ea648f185
stefanstifter <stefan.stifter@saswebdev.com>
2021-06-25 08:33:29 +0200

    Adapt dispatchFlashMessage hook for Edu project store logic

--- utils/useDispatchFlashMessage.js
+++ utils/useDispatchFlashMessage.js
@@ -1,10 +1,17 @@
 import { useDispatch } from 'react-redux';
-// import { enqueueSnackbar } from 'app/store/actions';
+import { enqueueSnackbar } from 'app/store/fuse/notificationSlice';
 
+/**
+ * @ToDo: For future usage in various projects we will accept `enqueSnackbar` as argument
+ * instead of importing it directly. This way the util can be used with
+ * different types of "store" logic.
+ *
+ * @returns function
+ */
 const useDispatchFlashMessage = () => {
     const dispatch = useDispatch();
     const dispatchFlashMessage = (message, variant = 'error') => {
-        // dispatch(enqueueSnackbar({ message, options: {variant} }));
+        dispatch(enqueueSnackbar({ message, options: {variant} }));
     }
 
     return dispatchFlashMessage;
