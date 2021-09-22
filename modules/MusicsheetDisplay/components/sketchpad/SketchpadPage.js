1a550822d1187852f756d2d7da9d9d7d4ac2bdaa
stefanstifter <stefan.stifter@saswebdev.com>
2021-09-22 11:24:47 +0200

    sketchpad: implement basic draw functionality utilizing canvas-draw dependency

--- modules/MusicsheetDisplay/components/sketchpad/SketchpadPage.js
+++ modules/MusicsheetDisplay/components/sketchpad/SketchpadPage.js
@@ -1,33 +1,37 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import PageLayerBlank from './PageLayerBlank';
 import LayerImagesPerPage from './LayerImagesPerPage';
 import PageLayerModeControl from './PageLayerModeControl';
 import { SketchpadContext } from '../../context/SketchpadContexts';
+import CanvasDraw from 'react-canvas-draw';
 
 const SketchpadPage = props => {
     const [layerOptions, setLayerOptions] = useState(null);
+    const [pageDimensions, setPageDimensions] = useState(null);
     const { isCreateActive } = useContext(SketchpadContext);
-    const imageEl = useRef();
 
-    // @ToDo: #59 sketchpad: resize all layer images to original image width before submit
-    console.log('image el', {
-        imageEl,
-        width: imageEl.current ? imageEl.current.width : '',
-        height: imageEl.current ? imageEl.current.height : '',
-        natWidth: imageEl.current ? imageEl.current.naturalWidth : '',
-        natHeight: imageEl.current ? imageEl.current.naturalHeight : '',
-    });
+    useEffect(() => {
+        // determine page images original size
+        const img = new Image();
+        img.src = props.page.downloadLink;
+        img.onload = () =>
+            setPageDimensions({
+                width: img.naturalWidth,
+                height: img.naturalHeight,
+            });
+    }, []);
 
     return (
         <div className="mt-24 border-b">
             <PageLayerModeControl handleLayerOptionsChange={option => setLayerOptions(option)} />
-            <div className="relative">
-                <img ref={imageEl} src={props.page.downloadLink} className="w-full" />
-
-                <LayerImagesPerPage page={props.page} />
-
-                {isCreateActive && layerOptions && <PageLayerBlank page={props.page} mode={layerOptions} />}
-            </div>
+            {isCreateActive && layerOptions && pageDimensions && (
+                <CanvasDraw
+                    imgSrc={props.page.downloadLink}
+                    canvasWidth={pageDimensions.width}
+                    canvasHeight={pageDimensions.height}
+                    hideGrid={true}
+                />
+            )}
             <p className="text-right">Seite {props.page.pageIndex + 1}</p>
         </div>
     );
