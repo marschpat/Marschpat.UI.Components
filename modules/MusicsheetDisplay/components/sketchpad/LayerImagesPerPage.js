0cb53f466955194e41e7ec14207f9ca79aad2877
stefanstifter <stefan.stifter@saswebdev.com>
2021-09-23 10:18:35 +0200

    Display only active layers in MusicsheetPageImageCarousel

--- modules/MusicsheetDisplay/components/sketchpad/LayerImagesPerPage.js
+++ modules/MusicsheetDisplay/components/sketchpad/LayerImagesPerPage.js
@@ -1,25 +1,16 @@
 import React, { useContext } from 'react';
 import { MusicsheetDisplayContext } from '../../context/MusicsheetDisplayContexts';
-// import { SketchpadContext } from '../../context/SketchpadContexts';
 
 const LayerImagesPerPage = props => {
     const { sketchpadLayers } = useContext(MusicsheetDisplayContext);
-
-    console.log('current', props.page.pageIndex);
-    // return sketchpadLayers.map(layer => <img src={layer.data[0].data} className="absolute inset-0" key={layer.uuid} />);
 
     function getCurrentPageFromLayer(layer) {
         return layer.data.find(i => i.pageIndex === props.page.pageIndex);
     }
 
-    return (
-        sketchpadLayers
-            .filter(layer => getCurrentPageFromLayer(layer))
-            // .filter(layer => layer.active && getCurrentPageFromLayer(layer))
-            .map(layer => (
-                <img src={getCurrentPageFromLayer(layer).data} className="absolute inset-0" key={layer.uuid} />
-            ))
-    );
+    return sketchpadLayers
+        .filter(layer => layer.active && getCurrentPageFromLayer(layer))
+        .map(layer => <img src={getCurrentPageFromLayer(layer).data} className="absolute inset-0" key={layer.uuid} />);
 };
 
 export default LayerImagesPerPage;
