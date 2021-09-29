2936b177e9aab0b8d5f18b805cedc11dab3618be
stefanstifter <stefan.stifter@saswebdev.com>
2021-09-29 15:14:49 +0200

    Provide IconButton and all necessary state within MusicsheetInfoPopover for higher extraction level in components lib

--- components/MusicsheetInfoPopover.js
+++ components/MusicsheetInfoPopover.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Icon from '@material-ui/core/Icon';
 import Popover from '@material-ui/core/Popover';
+import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 
 function MusicsheetInfoPopover(props) {
-    const [open, setOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
     const infoRows = [
         { name: 'Titel', value: props.musicsheet?.name ?? null },
@@ -14,39 +16,47 @@ function MusicsheetInfoPopover(props) {
         { name: 'Copyright', value: props.musicsheet?.copyright ?? null },
         { name: 'Untertitel', value: props.musicsheet?.subTitle ?? null },
     ];
-
-    useEffect(() => {
-        if (props.target) {
-            setOpen(true);
-            setAnchorEl(props.target);
-        }
-    }, [props.target]);
-
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
     const handleClose = () => {
-        setOpen(false);
         setAnchorEl(null);
-        props.handlePopoverReset();
     };
 
     return props.musicsheet ? (
-        <Popover
-            id="music-information"
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'center',
-            }}
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-            }}
-        >
-            <div className="p-20">
-                {infoRows.map((row, index) => row.value && <InfoRow name={row.name} value={row.value} key={index} />)}
-            </div>
-        </Popover>
+        <div className={props.className ?? ''}>
+            <Tooltip title="Infos zu dem Stück">
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="information"
+                    aria-describedby={'music-information'}
+                    onClick={handleClick}
+                >
+                    <Icon>info</Icon>
+                </IconButton>
+            </Tooltip>
+            <Popover
+                id="music-information"
+                open={!!anchorEl}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <div className="p-20">
+                    {infoRows.map(
+                        (row, index) => row.value && <InfoRow name={row.name} value={row.value} key={index} />
+                    )}
+                </div>
+            </Popover>
+        </div>
     ) : (
         <></>
     );
--- modules/MusicsheetDisplay/components/MusicsheetTitleInfo.js
+++ modules/MusicsheetDisplay/components/MusicsheetTitleInfo.js
@@ -1,14 +1,10 @@
 import React, { useContext, useState } from 'react';
 import MusicsheetInfoPopover from '@marschpat/Marschpat.UI.Components/components/MusicsheetInfoPopover';
 import { MusicsheetLoaderContext } from '../context/MusicsheetDisplayContexts';
-import Icon from '@material-ui/core/Icon';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 
 const MusicsheetTitleInfo = () => {
     const { musicsheetMetaData } = useContext(MusicsheetLoaderContext);
-    const [showPopover, setShowPopover] = useState(false);
 
     return (
         <div className="flex items-center">
@@ -16,23 +12,7 @@ const MusicsheetTitleInfo = () => {
                 Stück: {musicsheetMetaData.name}
             </Typography>
 
-            <Tooltip title="Musikstück informationen" className="ml-20">
-                <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="information"
-                    aria-describedby={'music-information'}
-                    onClick={e => setShowPopover(e.currentTarget)}
-                >
-                    <Icon>info</Icon>
-                </IconButton>
-            </Tooltip>
-
-            <MusicsheetInfoPopover
-                musicsheet={musicsheetMetaData}
-                target={showPopover}
-                handlePopoverReset={() => setShowPopover(false)}
-            />
+            <MusicsheetInfoPopover musicsheet={musicsheetMetaData} className="ml-20" />
         </div>
     );
 };
