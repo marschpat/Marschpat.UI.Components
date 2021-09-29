cfeb4e66232343025d973375e818cb3ab54f4354
stefanstifter <stefan.stifter@saswebdev.com>
2021-09-29 22:01:20 +0200

    Implement dirty (but working) poc: render sketchpad layers accurate in hacked ImageGallery. contributes to #34 #30

--- modules/MusicsheetDisplay/components/MusicsheetPageImageCarousel.js
+++ modules/MusicsheetDisplay/components/MusicsheetPageImageCarousel.js
@@ -6,41 +6,70 @@ import LayerImagesPerPage from './sketchpad/LayerImagesPerPage';
 import MusicsheetPagesLoader from './MusicsheetPagesLoader';
 
 const MusicsheetPageImageCarousel = () => {
-    const ImageGalleryEl = useRef();
-    const { musicsheetPages } = useContext(MusicsheetLoaderContext);
-    const { isCarouselFullscreen, setIsCarouselFullscreen, showPagesPreview } = useContext(MusicsheetDisplayContext);
+    const imageGalleryEl = useRef();
     const [pageImages, setPageImages] = useState([]);
     const [currentPageIndex, setCurrentPageIndex] = useState(0);
+    const { musicsheetPages } = useContext(MusicsheetLoaderContext);
+    const { isCarouselFullscreen, setIsCarouselFullscreen, showPagesPreview, sketchpadLayers } =
+        useContext(MusicsheetDisplayContext);
+
+    let imagesLoadedCount = 0;
 
     useEffect(() => {
         const images = musicsheetPages.map(item => ({ original: item.downloadLink, thumbnail: item.downloadLink }));
         setPageImages(images);
-
-        // const slideWrapper = ImageGalleryEl.current && ImageGalleryEl.current.imageGallerySlideWrapper.current;
-        // if (slideWrapper) {
-        //     console.log('slidewrapper', slideWrapper);
-        //     slideWrapper.className = 'relative';
-
-        //     const container = document.createElement('div');
-        //     container.className = 'absolute inset-0';
-        //     container.style.backgroundColor = '#3f3f3f';
-
-        //     slideWrapper.insertBefore(container, null);
-        // }
     }, [musicsheetPages]);
 
     useEffect(() => {
         if (isCarouselFullscreen) {
-            ImageGalleryEl.current.fullScreen();
+            imageGalleryEl.current.fullScreen();
         }
     }, [isCarouselFullscreen]);
+
+    function initializeSketchpadLayers(event) {
+        imagesLoadedCount += 1;
+        console.log('images loaded', imagesLoadedCount);
+        if (allImagesLoaded()) {
+            console.log('everything loaded, initialize Sketchpad Layers');
+            const slideWrapper = imageGalleryEl.current && imageGalleryEl.current.imageGallerySlideWrapper.current;
+            if (slideWrapper && sketchpadLayers.length > 0) {
+                const container = document.createElement('div');
+                const layerWrapper = document.createElement('div');
+
+                container.className = 'absolute inset-0';
+
+                const layers = sketchpadLayers
+                    // .filter(layer => layer.active && getCurrentPageFromLayer(layer))
+                    .map(layer => {
+                        const img = document.createElement('img');
+                        img.src = getCurrentPageFromLayer(layer).data;
+                        img.className = 'absolute top-0';
+                        return img;
+                    });
+
+                layers.forEach(layer => {
+                    layerWrapper.appendChild(layer);
+                });
+                container.appendChild(layerWrapper);
+                slideWrapper.insertBefore(container, null);
+            }
+        }
+    }
+
+    function getCurrentPageFromLayer(layer) {
+        return layer.data.find(i => i.pageIndex === currentPageIndex);
+    }
+
+    function allImagesLoaded() {
+        return pageImages.length === imagesLoadedCount;
+    }
 
     return (
         <MusicsheetPagesLoader>
             <div className="relative">
                 <ImageGallery
                     items={pageImages}
-                    ref={ImageGalleryEl}
+                    ref={imageGalleryEl}
                     showIndex={true}
                     thumbnailPosition="left"
                     showThumbnails={showPagesPreview}
@@ -48,13 +77,15 @@ const MusicsheetPageImageCarousel = () => {
                     onBeforeSlide={nextIndex => setCurrentPageIndex(null)}
                     onSlide={currentIndex => setCurrentPageIndex(currentIndex)}
                     onErrorImageURL="/assets/images/musiclibrary/IMAGE_ERROR_1.jpg"
+                    onImageLoad={initializeSketchpadLayers}
                 />
-                <div
+                {/* {container && <LayerImagesPerPage page={{ pageIndex: currentPageIndex }} container={container} />} */}
+                {/* <div
                     className="mx-auto absolute top-0 flex justify-center"
                     style={{ width: 'calc(100% - 110px)', marginLeft: '91px' }}
                 >
                     <LayerImagesPerPage page={{ pageIndex: currentPageIndex }} />
-                </div>
+                </div> */}
             </div>
         </MusicsheetPagesLoader>
     );
