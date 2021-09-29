7a663a813f7b9cf445ece6a9b2efd5a893de963a
stefanstifter <stefan.stifter@saswebdev.com>
2021-09-29 15:22:43 +0200

    Clean up

--- modules/MusicsheetDisplay/components/MusicsheetTitleInfo.js
+++ modules/MusicsheetDisplay/components/MusicsheetTitleInfo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import MusicsheetInfoPopover from '@marschpat/Marschpat.UI.Components/components/MusicsheetInfoPopover';
 import { MusicsheetLoaderContext } from '../context/MusicsheetDisplayContexts';
 import Typography from '@material-ui/core/Typography';
