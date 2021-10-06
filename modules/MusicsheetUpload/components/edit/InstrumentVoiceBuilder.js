7c245a930d4e504d4c3b6156eee5ca30710846e5
stefanstifter <stefan.stifter@saswebdev.com>
2021-10-06 17:13:13 +0200

    MusicsheetUploader: scaffold voice builder ui with mui selects

--- modules/MusicsheetUpload/components/edit/InstrumentVoiceBuilder.js
+++ modules/MusicsheetUpload/components/edit/InstrumentVoiceBuilder.js
@@ -1,7 +1,64 @@
 import React from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
 
 const InstrumentVoiceBuilder = props => {
-    return <div>new InstrumentVoiceBuilder</div>;
+    const age = 20;
+    return (
+        <div className="mt-20">
+            {/* INSTRUMENT */}
+            <div>
+                <Typography variant="h6">Instrument auswählen *</Typography>
+                <FormControl fullWidth>
+                    <InputLabel id="variant-clef">Instrument</InputLabel>
+                    <Select
+                        labelId="variant-clef"
+                        value=""
+                        label="Instrument"
+                        variant="outlined"
+                        // onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+
+            {/* MOOO */}
+            <FormControl fullWidth>
+                <InputLabel id="mood-clef">Stimmung</InputLabel>
+                <Select
+                    labelId="mood-clef"
+                    value=""
+                    label="Stimmung"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+            {/* VARIANT */}
+            <FormControl fullWidth>
+                <InputLabel id="variant-clef">Variante</InputLabel>
+                <Select
+                    labelId="variant-clef"
+                    value=""
+                    label="variant"
+                    // onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+    );
 };
 
 export default InstrumentVoiceBuilder;
