adf9924f9d545bca42387567c11251b4adbd450e
stefanstifter <stefan.stifter@saswebdev.com>
2021-10-06 17:50:05 +0200

    MusicsheetUploader: fetch new voice parts (clef, instrument-new, instrument-voice), marschpat/Marschpat.Edu.UI.Web#66

--- modules/MusicsheetUpload/components/edit/InstrumentVoiceBuilder.js
+++ modules/MusicsheetUpload/components/edit/InstrumentVoiceBuilder.js
@@ -4,59 +4,34 @@ import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
+import ChooseOrCreateSelector from '@marschpat/Marschpat.UI.Components/components/ChooseOrCreateSelector';
 
 const InstrumentVoiceBuilder = props => {
-    const age = 20;
     return (
         <div className="mt-20">
-            {/* INSTRUMENT */}
-            <div>
-                <Typography variant="h6">Instrument auswählen *</Typography>
-                <FormControl fullWidth>
-                    <InputLabel id="variant-clef">Instrument</InputLabel>
-                    <Select
-                        labelId="variant-clef"
-                        value=""
-                        label="Instrument"
-                        variant="outlined"
-                        // onChange={handleChange}
-                    >
-                        <MenuItem value={10}>Ten</MenuItem>
-                        <MenuItem value={20}>Twenty</MenuItem>
-                        <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-            </div>
+            {/* CLEF */}
+            <ChooseOrCreateSelector
+                label="Stimmung"
+                fetchOptionsUrl="/clef"
+                labelAttr="name"
+                handleSelectedChange={e => console.log('changed', e)}
+            />
 
-            {/* MOOO */}
-            <FormControl fullWidth>
-                <InputLabel id="mood-clef">Stimmung</InputLabel>
-                <Select
-                    labelId="mood-clef"
-                    value=""
-                    label="Stimmung"
-                    // onChange={handleChange}
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-            </FormControl>
+            {/* INSTRUMENT */}
+            <ChooseOrCreateSelector
+                label="Instrument"
+                fetchOptionsUrl="/instrument-new"
+                labelAttr="name"
+                handleSelectedChange={e => console.log('changed', e)}
+            />
 
             {/* VARIANT */}
-            <FormControl fullWidth>
-                <InputLabel id="variant-clef">Variante</InputLabel>
-                <Select
-                    labelId="variant-clef"
-                    value=""
-                    label="variant"
-                    // onChange={handleChange}
-                >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-            </FormControl>
+            <ChooseOrCreateSelector
+                label="Stimmung"
+                fetchOptionsUrl="/instrument-voice"
+                labelAttr="name"
+                handleSelectedChange={e => console.log('changed', e)}
+            />
         </div>
     );
 };
