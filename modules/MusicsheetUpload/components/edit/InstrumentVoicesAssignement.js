d909c8ba8af3ed3252fd7a5c1dd2c35755932ece
stefanstifter <stefan.stifter@saswebdev.com>
2021-10-06 13:05:27 +0200

    Uploader: reactor InstrumentVoicesAssignement, extract functionallity into InstrumentVoicesSelector

--- modules/MusicsheetUpload/components/edit/InstrumentVoicesAssignement.js
+++ modules/MusicsheetUpload/components/edit/InstrumentVoicesAssignement.js
@@ -1,72 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { UploaderContext } from '../../context/UploaderContext';
-import FuseChipSelect from '@fuse/core/FuseChipSelect';
-import Typography from '@material-ui/core/Typography';
+import React, { useContext } from 'react';
 import { MP_EDU } from '../../utils/ImplementationModesLookup';
+import { UploaderContext } from '../../context/UploaderContext';
+import InstrumentVoicesSelector from './InstrumentVoicesSelector';
+import Typography from '@material-ui/core/Typography';
 
 const InstrumentVoicesAssignement = props => {
-    const [availableVoices, setAvailableVoices] = useState(null);
-    const [showWarning, setShowWarning] = useState(null);
-    const { availableInstrumentVoices, implementationMode } = useContext(UploaderContext);
-
-    const handleChange = (values, detail) => {
-        // if removed value isn't included in availableVoices yet, reinclude it
-        if (detail.action === 'remove-value') {
-            setAvailableVoices(prev => {
-                let newAvailable = prev;
-                const removedValue = detail.removedValue;
-                if (
-                    removedValue &&
-                    removedValue.voiceID &&
-                    !newAvailable.find(item => item.voiceID === removedValue.voiceID)
-                ) {
-                    newAvailable.push(removedValue);
-                    newAvailable = newAvailable.sort(compareByVoiceId);
-                }
-                return newAvailable;
-            });
-        }
-        props.handleVoicesAssignemnt(values);
-    };
-
-    useEffect(() => {
-        if (!availableInstrumentVoices) {
-            setShowWarning(true);
-        }
-        if (availableInstrumentVoices) {
-            setAvailableVoices(availableInstrumentVoices);
-        }
-    }, [availableInstrumentVoices]);
+    const { implementationMode } = useContext(UploaderContext);
 
     return (
         <div className="w-full">
             <Typography variant="h5">
                 {implementationMode === MP_EDU ? 'Stimme festlegen' : 'Stimme auswählen'}
             </Typography>
-            <FuseChipSelect
-                className="mt-8"
-                value={props.assignedVoices}
-                onChange={handleChange}
-                placeholder="Instrumentenstimme wählen"
-                textFieldProps={{ variant: 'standard' }}
-                options={availableVoices}
-                isMulti
-                variant="fixed"
-                id="voices-assignment"
+
+            <InstrumentVoicesSelector
+                assignedVoices={props.assignedVoices}
+                handleVoicesAssignemnt={props.handleVoicesAssignemnt}
             />
-            {showWarning && (
-                <div className="mt-24 py-4 flex justify-center text-xl rounded-md bg-orange-700">
-                    <span>! Bitte zuerst Besetzung auswählen !</span>
-                </div>
-            )}
         </div>
     );
 };
-
-function compareByVoiceId(itemA, itemB) {
-    if (itemA.voiceID < itemB.voiceID) return -1;
-    if (itemA.voiceID > itemB.voiceID) return 1;
-    return 0;
-}
 
 export default InstrumentVoicesAssignement;
---
+++ modules/MusicsheetUpload/components/edit/InstrumentVoicesSelector.js
@@ -0,0 +1,67 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { UploaderContext } from '../../context/UploaderContext';
+import FuseChipSelect from '@fuse/core/FuseChipSelect';
+
+const InstrumentVoicesSelector = props => {
+    const [availableVoices, setAvailableVoices] = useState(null);
+    const [showWarning, setShowWarning] = useState(null);
+    const { availableInstrumentVoices } = useContext(UploaderContext);
+
+    useEffect(() => {
+        if (!availableInstrumentVoices) {
+            setShowWarning(true);
+        }
+        if (availableInstrumentVoices) {
+            setAvailableVoices(availableInstrumentVoices);
+        }
+    }, [availableInstrumentVoices]);
+
+    const handleChange = (values, detail) => {
+        // if removed value isn't included in availableVoices yet, reinclude it
+        if (detail.action === 'remove-value') {
+            setAvailableVoices(prev => {
+                let newAvailable = prev;
+                const removedValue = detail.removedValue;
+                if (
+                    removedValue &&
+                    removedValue.voiceID &&
+                    !newAvailable.find(item => item.voiceID === removedValue.voiceID)
+                ) {
+                    newAvailable.push(removedValue);
+                    newAvailable = newAvailable.sort(compareByVoiceId);
+                }
+                return newAvailable;
+            });
+        }
+        props.handleVoicesAssignemnt(values);
+    };
+
+    return (
+        <>
+            <FuseChipSelect
+                className="mt-8"
+                value={props.assignedVoices}
+                onChange={handleChange}
+                placeholder="Instrumentenstimme wählen"
+                textFieldProps={{ variant: 'standard' }}
+                options={availableVoices}
+                isMulti
+                variant="fixed"
+                id="voices-assignment"
+            />
+            {showWarning && (
+                <div className="mt-24 py-4 flex justify-center text-xl rounded-md bg-orange-700">
+                    <span>! Bitte zuerst Besetzung auswählen !</span>
+                </div>
+            )}
+        </>
+    );
+
+    function compareByVoiceId(itemA, itemB) {
+        if (itemA.voiceID < itemB.voiceID) return -1;
+        if (itemA.voiceID > itemB.voiceID) return 1;
+        return 0;
+    }
+};
+
+export default InstrumentVoicesSelector;
