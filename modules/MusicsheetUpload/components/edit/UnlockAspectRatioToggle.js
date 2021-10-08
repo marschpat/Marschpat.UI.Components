fe79bfa98d2c7f639f6c8da2b2a9541f80524441
stefanstifter <stefan.stifter@saswebdev.com>
2021-10-08 14:15:30 +0200

    MusicsheetUpload: toggle UnlockAspectRatio text, marschpat/Marschpat.Edu.UI.Web#63

--- modules/MusicsheetUpload/components/edit/UnlockAspectRatioToggle.js
+++ modules/MusicsheetUpload/components/edit/UnlockAspectRatioToggle.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TooltipStyled from '@marschpat/Marschpat.UI.Components/components/TooltipStyled';
 import { Checkbox } from '@material-ui/core';
 import AspectRatioIcon from '@material-ui/icons/AspectRatio';
 
 const UnlockAspectRatioToggle = props => {
+    const [checked, setChecked] = useState(true);
     return (
-        <TooltipStyled title="Seitenverhältnis entsperren">
-            <Checkbox checked={true} icon={<AspectRatioIcon />} checkedIcon={<AspectRatioIcon />} />
+        <TooltipStyled title={checked ? 'Seitenverhältnis entsperren' : 'Seitenverhältnis sperren'}>
+            <Checkbox
+                checked={checked}
+                onChange={() => setChecked(prev => !prev)}
+                icon={<AspectRatioIcon />}
+                checkedIcon={<AspectRatioIcon />}
+            />
         </TooltipStyled>
     );
 };
