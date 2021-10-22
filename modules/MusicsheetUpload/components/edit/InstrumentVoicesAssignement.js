074ac1ca2d89c243d45a1fcfe83cb22682819287
stefanstifter <stefan.stifter@saswebdev.com>
2021-10-22 15:11:20 +0200

    Prepare merge

---
+++ modules/MusicsheetUpload/components/edit/InstrumentVoicesAssignement.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { UploaderContext } from '../../context/UploaderContext';
+import InstrumentVoicesSelector from './InstrumentVoicesSelector';
+import InstrumentVoiceBuilder from './InstrumentVoiceBuilder';
+import { MP_EDU, MP_WEB } from '@marschpat/Marschpat.UI.Components/utils/ImplementationModesLookup';
+import Typography from '@material-ui/core/Typography';
+
+const InstrumentVoicesAssignement = props => {
+    const { implementationMode } = useContext(UploaderContext);
+
+    return (
+        <div className="w-full">
+            <Typography variant="h5">
+                {implementationMode === MP_EDU
+                    ? 'Stimme & Instrument festlegen'
+                    : 'Stimme auswählen'}
+            </Typography>
+
+            {implementationMode === MP_WEB && (
+                <InstrumentVoicesSelector
+                    assignedVoices={props.assignedVoices}
+                    handleVoicesAssignemnt={props.handleVoicesAssignemnt}
+                />
+            )}
+
+            {implementationMode === MP_EDU && (
+                <InstrumentVoiceBuilder
+                    assignedVoices={props.assignedVoices}
+                    handleVoicesAssignemnt={props.handleVoicesAssignemnt}
+                />
+            )}
+        </div>
+    );
+};
+
+export default InstrumentVoicesAssignement;
