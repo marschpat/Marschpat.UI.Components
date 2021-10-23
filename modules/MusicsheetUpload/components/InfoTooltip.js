025d0753309418bfa74565be6cc5588ddba026c9
stefanstifter <stefan.stifter@saswebdev.com>
2021-10-23 10:38:42 +0200

    MusicsheetUpload: fix help descriptions on safari mobile

--- modules/MusicsheetUpload/components/InfoTooltip.js
+++ modules/MusicsheetUpload/components/InfoTooltip.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import TooltipStyled from '@marschpat/Marschpat.UI.Components/components/TooltipStyled';
 import EmojiObjectsOutlinedIcon from '@material-ui/icons/EmojiObjectsOutlined';
 
 function InfoTooltip(props) {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={props.className ?? ''}>
-            <TooltipStyled title={props.title ?? ''}>
+            <TooltipStyled isOpen={isOpen} setIsOpen={setIsOpen} title={props.title ?? ''}>
                 <IconButton
                     className="text-orange-300"
                     color="inherit"
                     aria-label={props.name ?? 'information'}
                     size="small"
+                    onClick={() => setIsOpen(prev => !prev)}
                 >
                     <EmojiObjectsOutlinedIcon className="text-3xl" fontSize="inherit" />
                 </IconButton>
