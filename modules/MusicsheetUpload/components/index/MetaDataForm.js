57051698e77193fccae18943e11fd0f5ef40c166
stefanstifter <stefan.stifter@saswebdev.com>
2021-11-01 11:46:34 +0100

    MusicsheetUpload: for sanity reasons check for both tagID and tagId

--- modules/MusicsheetUpload/components/index/MetaDataForm.js
+++ modules/MusicsheetUpload/components/index/MetaDataForm.js
@@ -176,7 +176,7 @@ const MetaDataForm = props => {
             tags:
                 tags && tags.length > 0
                     ? tags.map(tag => {
-                          if (tag) return { tagId: tag.tagID };
+                          if (tag) return { tagID: tag.tagID };
                       })
                     : tags,
         });
