f21e62d857a4dbc100e1f4321b4061917997107d
stefanstifter <stefan.stifter@saswebdev.com>
2021-11-05 18:29:46 +0100

    MusicsheetUpload: show embed video button when in hellp mode and add tooltiop, close marschpat/Marschpat.UI.Web#864

--- modules/MusicsheetUpload/components/index/EmbedVideo.js
+++ modules/MusicsheetUpload/components/index/EmbedVideo.js
@@ -1,48 +1,55 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import { IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import TheatersIcon from '@material-ui/icons/Theaters';
+import { UploaderContext } from '../../context/UploaderContext';
+import TooltipStyled from '@marschpat/Marschpat.UI.Components/components/TooltipStyled';
 
 function EmbedVideo(props) {
     const [isOpen, setIsOpen] = useState(false);
+    const { inHelpMode } = useContext(UploaderContext);
 
     return (
-        <div>
-            <Button onClick={() => setIsOpen(true)}>
-                <div className="flex items-center">
-                    <TheatersIcon />
-                    <div className="ml-8">Video ansehen</div>
-                </div>
-            </Button>
-            <Modal
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
-                className="absolute p-20 flex items-center justify-center"
-                aria-labelledby="Wie funktioniert der Notenblatt Upload Video"
-            >
-                <div className="bg-gray-50 rounded-md w-full max-w-xl">
-                    <div className="flex justify-end">
-                        <IconButton onClick={() => setIsOpen(false)}>
-                            <CloseIcon />
-                        </IconButton>
+        inHelpMode && (
+            <div>
+                <TooltipStyled title="Wie funktioniert der Uploader">
+                    <Button onClick={() => setIsOpen(true)}>
+                        <div className="flex items-center text-base text-orange-300 font-bold">
+                            <TheatersIcon />
+                            <div className="ml-8">Video ansehen</div>
+                        </div>
+                    </Button>
+                </TooltipStyled>
+                <Modal
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
+                    className="absolute p-20 flex items-center justify-center"
+                    aria-labelledby="Wie funktioniert der Notenblatt Upload Video"
+                >
+                    <div className="bg-gray-50 rounded-md w-full max-w-xl">
+                        <div className="flex justify-end">
+                            <IconButton onClick={() => setIsOpen(false)}>
+                                <CloseIcon />
+                            </IconButton>
+                        </div>
+                        <div className="px-24 pb-24">
+                            <iframe
+                                width="1120"
+                                height="630"
+                                className="w-full"
+                                src="https://www.youtube.com/embed/wUd48D-hQ5M"
+                                title="YouTube video player"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
                     </div>
-                    <div className="px-24 pb-24">
-                        <iframe
-                            width="1120"
-                            height="630"
-                            className="w-full"
-                            src="https://www.youtube.com/embed/wUd48D-hQ5M"
-                            title="YouTube video player"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </div>
-                </div>
-            </Modal>
-        </div>
+                </Modal>
+            </div>
+        )
     );
 }
 
