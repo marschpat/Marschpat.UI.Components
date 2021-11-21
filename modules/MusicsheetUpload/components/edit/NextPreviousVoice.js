639f0bc6bd3aa3d3d0fb1698cdd3fafaa7fc9a2d
stefanstifter <stefan.stifter@saswebdev.com>
2021-11-21 15:39:19 +0100

    MusicsheetUpload: debounce "next / previous" button click, try to prevent editor modal crashing when mashing buttons, marschpat/Marschpat.UI.Web/issues#877

--- modules/MusicsheetUpload/components/edit/NextPreviousVoice.js
+++ modules/MusicsheetUpload/components/edit/NextPreviousVoice.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 
 const NextPreviousVoice = props => {
+    const [disabled, setDisabled] = useState({ next: false, prev: false });
+    const buttonEl = useRef(null);
+
     return (
         <div className="flex justify-end">
             <Button
-                onClick={e => props.handleNextInstrumentSheet(e, 'previous')}
+                onClick={e => {
+                    setTimeout(() => {
+                        buttonEl.current.setAttribute('disabled', 'disabled');
+                        setDisabled({ ...disabled, prev: true });
+                        props.handleNextInstrumentSheet(e, 'previous');
+                    }, 250);
+                }}
                 variant="contained"
                 color="secondary"
                 className="text-white mr-10"
                 title="Stimme speichern und vorherige Stimme bearbeiten"
+                disabled={disabled.prev}
+                ref={buttonEl}
             >
                 <SkipPreviousIcon />
                 <span className="ml-10">Vorherige Stimme</span>
@@ -22,6 +33,7 @@ const NextPreviousVoice = props => {
                 color="secondary"
                 className="text-white ml-10"
                 title="Stimme speichern und nächste Stimme bearbeiten"
+                disabled={!props.isReady}
             >
                 <SkipNextIcon />
                 <span className="ml-10">Nächste Stimme</span>
