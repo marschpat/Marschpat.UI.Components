c836a95cfe168d6ac44aed1eee2a38cac5520884
stefanstifter <stefan.stifter@saswebdev.com>
2021-11-22 10:53:50 +0100

    MusicsheetUpload: cleanup, marschpat/Marschpat.UI.Web#877

--- modules/MusicsheetUpload/index.js
+++ modules/MusicsheetUpload/index.js
@@ -242,7 +242,6 @@ const MusicsheetUpload = ({ user, organisation, implementationMode, dispatchFlas
      * Open the next available instrumentSheet, depending on the direction (next or previous)
      */
     function openNextAvailableInstrumentSheet(direction) {
-        setOpenEdit(false);
         const existingIndex = instrumentSheets.findIndex(
             sheet => sheet.uuid === instrumentSheetInEdit.uuid
         );
@@ -250,7 +249,6 @@ const MusicsheetUpload = ({ user, organisation, implementationMode, dispatchFlas
         const fallbackIndex = direction === 'next' ? 0 : instrumentSheets.length - 1;
         const newIndex = instrumentSheets[candidate] ? candidate : fallbackIndex;
         setInstrumentSheetInEdit(instrumentSheets[newIndex]);
-        setOpenEdit(true);
     }
 };
 
