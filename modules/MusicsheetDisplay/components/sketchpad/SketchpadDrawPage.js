6985220ab66b92db4fce7d1d26c9ba58543d241a
stefanstifter <stefan.stifter@saswebdev.com>
2021-12-09 16:19:35 +0100

    MusicsheetDisplay: Show circular loading progress while fetching sketchpads pageimages

--- modules/MusicsheetDisplay/components/sketchpad/SketchpadDrawPage.js
+++ modules/MusicsheetDisplay/components/sketchpad/SketchpadDrawPage.js
@@ -1,22 +1,26 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
-import PageLayerModeControl from './PageLayerModeControl';
 import CanvasDraw from 'react-canvas-draw';
+import PageLayerModeControl from './PageLayerModeControl';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const SketchpadDrawPage = forwardRef((props, ref) => {
     const canvasDrawEl = useRef();
     const [layerOptions, setLayerOptions] = useState(null);
     const [pageDimensions, setPageDimensions] = useState(null);
+    const [pageImage, setPageImage] = useState(null);
 
     useEffect(() => {
         // determine page images original size
         // @ToDo find a better way to do this!
         const img = new Image();
         img.src = props.page.downloadLink;
-        img.onload = () =>
+        img.onload = () => {
             setPageDimensions({
                 width: img.naturalWidth,
                 height: img.naturalHeight,
             });
+            setPageImage(img);
+        };
     }, []);
 
     // expose function to ref
@@ -51,17 +55,22 @@ const SketchpadDrawPage = forwardRef((props, ref) => {
                 handleLayerOptionsChange={option => setLayerOptions(option)}
                 handleUndoDraw={undoDraw}
             />
-            {layerOptions && pageDimensions && (
+            {layerOptions && pageImage && (
                 <div className="py-24 w-full flex justify-center">
                     <CanvasDraw
                         ref={canvasDrawEl}
-                        imgSrc={props.page.downloadLink}
+                        imgSrc={pageImage.src}
                         canvasWidth={pageDimensions.width}
                         canvasHeight={pageDimensions.height}
                         brushColor={layerOptions.color}
                         brushRadius={layerOptions.size}
                         hideGrid={true}
                     />
+                </div>
+            )}
+            {!pageImage && (
+                <div className="flex justify-center">
+                    <CircularProgress />
                 </div>
             )}
             <div className="mb-12 text-right text-gray-700">Seite {props.page.pageIndex + 1}</div>
