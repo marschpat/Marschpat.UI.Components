0eb27a2cc41ce82d010f506e895d109b3565daea
stefanstifter <stefan.stifter@saswebdev.com>
2022-01-07 09:21:12 +0100

    Sketchpad: try to fetch pageImage dimensions with new s3 link

--- modules/MusicsheetDisplay/components/sketchpad/SketchpadDrawPage.js
+++ modules/MusicsheetDisplay/components/sketchpad/SketchpadDrawPage.js
@@ -2,25 +2,17 @@ import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } f
 import CanvasDraw from 'react-canvas-draw';
 import PageLayerModeControl from './PageLayerModeControl';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios';
 
 const SketchpadDrawPage = forwardRef((props, ref) => {
     const canvasDrawEl = useRef();
     const [layerOptions, setLayerOptions] = useState(null);
     const [pageDimensions, setPageDimensions] = useState(null);
-    const [pageImage, setPageImage] = useState(null);
 
+    // fetch pageimage for pageIndex again to determine original image dimensions
+    // @ToDo double requests: find a better way to do this!
     useEffect(() => {
-        // determine page images original size
-        // @ToDo find a better way to do this!
-        const img = new Image();
-        img.src = props.page.downloadLink;
-        img.onload = () => {
-            setPageDimensions({
-                width: img.naturalWidth,
-                height: img.naturalHeight,
-            });
-            setPageImage(img);
-        };
+        fetchImageDimensions();
     }, []);
 
     // expose function to ref
@@ -36,6 +28,32 @@ const SketchpadDrawPage = forwardRef((props, ref) => {
             };
         },
     }));
+
+    async function fetchImageDimensions() {
+        const { success, data } = await fetchMusicsheetVoicePage(props.page.musicSheetId, props.page.voiceId, props.page.pageIndex);
+        if (success) {
+            console.log('neww datas', data);
+            const img = new Image();
+            img.src = data.downloadLink;
+            img.onload = () => {
+                setPageDimensions({ width: img.naturalWidth, height: img.naturalHeight });
+            }
+        }
+    }
+
+    async function fetchMusicsheetVoicePage(sheetId, voiceId, pageIndex) {
+        try {
+            const response = await axios.post(`musiclibrary/${sheetId}/download/${voiceId}/${pageIndex}`);
+            const success = response?.data ? true : false;
+            const data = success ? response.data : 'invalid API response (no data)';
+            return { success, data };
+        } catch (error) {
+            const errorMsg = error?.response?.data?.message;
+            console.error('Error while fechtching musicsheet downloadLink occured:', errorMsg);
+
+            return { success: false, data: errorMsg };
+        }
+    }
 
     function isCanvasBlank() {
         const canvas = canvasDrawEl.current.canvasContainer.children[1];
@@ -55,11 +73,11 @@ const SketchpadDrawPage = forwardRef((props, ref) => {
                 handleLayerOptionsChange={option => setLayerOptions(option)}
                 handleUndoDraw={undoDraw}
             />
-            {layerOptions && pageImage && (
+            {layerOptions && pageDimensions && (
                 <div className="py-24 w-full flex justify-center">
                     <CanvasDraw
                         ref={canvasDrawEl}
-                        imgSrc={pageImage.src}
+                        imgSrc={props.page.downloadLink}
                         canvasWidth={pageDimensions.width}
                         canvasHeight={pageDimensions.height}
                         brushColor={layerOptions.color}
@@ -69,7 +87,7 @@ const SketchpadDrawPage = forwardRef((props, ref) => {
                     />
                 </div>
             )}
-            {!pageImage && (
+            {!pageDimensions && (
                 <div className="flex justify-center">
                     <CircularProgress />
                 </div>
