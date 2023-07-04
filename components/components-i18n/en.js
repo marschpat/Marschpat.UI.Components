10b9bbf5e949c5668e9503270e8c0849b1b5c196
CINIMOD <dschleger@gmail.com>
2023-07-04 19:05:07 +0200

    import translation files

--- components/components-i18n/en.js
+++ components/components-i18n/en.js
@@ -1,14 +1,14 @@
 const locale = {
-    COMP_POPOVER_TITLE: 'Titel',
-    COMP_POPOVER_SUBTITLE: 'Untertitel',
-    COMP_POPOVER_COMPOSER: 'Komponist',
-    COMP_POPOVER_PUBLISHER: 'Verlag',
-    COMP_POPOVER_ARRANGEUR: 'Arrangeur',
-    COMP_POPOVER_CAST: 'Besetzung',
-    COMP_POPOVER_ISWC_NR: 'ISWC Nummer',
+    COMP_POPOVER_TITLE: 'Title',
+    COMP_POPOVER_SUBTITLE: 'Subtitle',
+    COMP_POPOVER_COMPOSER: 'Composer',
+    COMP_POPOVER_PUBLISHER: 'Publisher',
+    COMP_POPOVER_ARRANGEUR: 'Arranger',
+    COMP_POPOVER_CAST: 'Instrumentation',
+    COMP_POPOVER_ISWC_NR: 'ISWC number',
     COMP_POPOVER_COPYRIGHT: 'Copyright',
-    COMP_POPOVER_TTP: 'Infos zu dem Stück',
-    COMP_POPOVER_HEADER: 'Informationen zum Musikstück',
+    COMP_POPOVER_TTP: 'Information about the music sheet',
+    COMP_POPOVER_HEADER: 'Information about the music sheet',
 };
 
 export default locale;
